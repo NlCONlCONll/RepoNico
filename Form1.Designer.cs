7125e3a044d9616822d05722778d3bda21d64d83
Nico Prasetyo <prasetyon100402@gmail.com>
2020-12-18 08:40:45 +0700

    Code

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.buttonUrutkan = new System.Windows.Forms.Button();
+            this.buttonReverse = new System.Windows.Forms.Button();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Location = new System.Drawing.Point(13, 13);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(39, 17);
+            this.labelInput.TabIndex = 0;
+            this.labelInput.Text = "Input";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(16, 34);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(290, 22);
+            this.textBox.TabIndex = 1;
+            // 
+            // buttonUrutkan
+            // 
+            this.buttonUrutkan.Location = new System.Drawing.Point(160, 62);
+            this.buttonUrutkan.Name = "buttonUrutkan";
+            this.buttonUrutkan.Size = new System.Drawing.Size(146, 23);
+            this.buttonUrutkan.TabIndex = 2;
+            this.buttonUrutkan.Text = "URUTKAN";
+            this.buttonUrutkan.UseVisualStyleBackColor = true;
+            this.buttonUrutkan.Click += new System.EventHandler(this.buttonUrutkan_Click);
+            // 
+            // buttonReverse
+            // 
+            this.buttonReverse.Location = new System.Drawing.Point(16, 62);
+            this.buttonReverse.Name = "buttonReverse";
+            this.buttonReverse.Size = new System.Drawing.Size(125, 23);
+            this.buttonReverse.TabIndex = 3;
+            this.buttonReverse.Text = "REVERSE";
+            this.buttonReverse.UseVisualStyleBackColor = true;
+            this.buttonReverse.Click += new System.EventHandler(this.buttonReverse_Click);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(16, 119);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(51, 17);
+            this.labelOutput.TabIndex = 4;
+            this.labelOutput.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.buttonReverse);
+            this.Controls.Add(this.buttonUrutkan);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.labelInput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button buttonUrutkan;
+        private System.Windows.Forms.Button buttonReverse;
+        private System.Windows.Forms.Label labelOutput;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,46 @@ namespace _14__Praktikum
         {
             InitializeComponent();
         }
+
+        private void buttonReverse_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show("Tolong isi textBox dulu");
+            }
+            else
+            {
+                string mauReverse = textBox.Text;
+                char[] goReverse = mauReverse.ToCharArray(); 
+                string reverse = "";
+                for (int i = goReverse.Length - 1; i > -1; i--) 
+                {
+                    reverse = reverse + goReverse[i]; 
+                }
+                labelOutput.Text = reverse;
+            }
+        }
+
+        private void buttonUrutkan_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show("Tolong isi textBox dulu");
+            }
+            else
+            {
+                string mauUrut = textBox.Text;
+                char[] goUrut = mauUrut.ToCharArray();
+                Array.Sort(goUrut);
+                
+                string urut = "";
+                for(int j = 0; j < goUrut.Length; j++)
+                {
+                    urut = urut + goUrut[j];
+                }
+                labelOutput.Text = urut;
+            }
+        }
+
     }
 }
